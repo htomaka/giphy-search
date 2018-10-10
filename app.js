f6121bd32c62becc3fab53e856be0c6b90513369
Honore Tomaka <HTOMAKA@logic-immo.com>
2018-10-10 15:56:14 +0200

    add search

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 import express from 'express';
+import http from 'http';
+import hbs from 'express-handlebars';
+
 const app = express();
 
-app.get('/hello-world', function (req, res) {
-    res.send('Hello World');
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    const {query} = req;
+    const term = encodeURIComponent(query.term);
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC`;
+
+    http.get(url, response => {
+        let body = '';
+
+        response.on('data', data => {
+            console.log('on data', data);
+            body += data;
+        });
+
+        response.on('end', () => {
+            const parsed = JSON.parse(body);
+            res.render('home', {gifs: parsed.data});
+        })
+    });
+});
+
+app.get('/hello-gif', function (req, res) {
+    const gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
+    res.render('hello-gif', {gifUrl: gifUrl})
+});
+
+app.get('/greetings/:name', (req, res) => {
+    const {name} = req.params;
+    res.render('greetings', {name});
 });
 
 app.listen(3000, () => {
--- dist/app.js
+++ dist/app.js
@@ -2,11 +2,47 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _http = _interopRequireDefault(require("http"));
+
+var _expressHandlebars = _interopRequireDefault(require("express-handlebars"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express.default)();
-app.get('/hello-world', function (req, res) {
-  res.send('Hello World');
+app.engine('handlebars', (0, _expressHandlebars.default)({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.get('/', function (req, res) {
+  var query = req.query;
+  var term = encodeURIComponent(query.term);
+  var url = "http://api.giphy.com/v1/gifs/search?q=".concat(term, "&api_key=dc6zaTOxFJmzC");
+
+  _http.default.get(url, function (response) {
+    var body = '';
+    response.on('data', function (data) {
+      console.log('on data', data);
+      body += data;
+    });
+    response.on('end', function () {
+      var parsed = JSON.parse(body);
+      res.render('home', {
+        gifs: parsed.data
+      });
+    });
+  });
+});
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
+  res.render('hello-gif', {
+    gifUrl: gifUrl
+  });
+});
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {
+    name: name
+  });
 });
 app.listen(3000, function () {
   console.log('server listening on port localhost:3000');
